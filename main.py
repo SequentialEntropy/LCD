d2306ddcaa7260e827fddfadc85d5b2be22a7da4
Shadow711 <asahi79289@gmail.com>
2021-10-11 18:10:48 +0800

    Fix: Sync blinking output

--- main.py
+++ main.py
@@ -11,35 +11,44 @@ import stats
 
 async def display_date(display):
     while True:
-        await display.display(time.strftime("%a %d %b"), 1, reset=False)
+        date_out = time.strftime("%a %d %b")
+        await display.display(date_out, 1, reset=False)
         await asyncio.sleep(60)
 
 async def display_time(display):
     while True:
-        await display.display(time.strftime("%H:%M:%S"), 1, 20, alignRight=True, reset=False)
+        time_out = time.strftime("%H:%M:%S")
+        await display.display(time_out, 1, 20, alignRight=True, reset=False)
         await asyncio.sleep(1)
 
 async def display_online(display, server):
     while True:
-        await display.display("Players Online: {}".format(str(server.get_status()["players"]["online"])), 3)
+        players_out = "Players Online: {}".format(server.get_status()["players"]["online"])
+        await display.display(players_out, 3)
         await asyncio.sleep(0.1)
 
 async def display_cpu(display):
     while True:
-        await display.display("CPU: {}%".format(stats.percent_cpu()), 2)
-        await display.display("Temp: {}°C".format(stats.temp_cpu()), 2, 20, alignRight=True, reset=False)
+        percent_cpu_out = "CPU: {}%".format(stats.percent_cpu())
+        temp_cpu_out = "Temp: {}°C".format(stats.temp_cpu())
+        await display.display(percent_cpu_out, 2)
+        await display.display(temp_cpu_out, 2, 20, alignRight=True, reset=False)
         await asyncio.sleep(1)
 
 async def display_ram(display):
     while True:
-        await display.display("RAM: {}/{} GB".format(stats.used_ram(3), stats.total_ram()), 3)
-        await display.display("{}%".format(stats.percent_ram()), 3, 20, alignRight=True, reset=False)
+        ratio_ram_out = "RAM: {}/{} GB".format(stats.used_ram(3), stats.total_ram())
+        percent_ram_out = "{}%".format(stats.percent_ram())
+        await display.display(ratio_ram_out, 3)
+        await display.display(percent_ram_out, 3, 20, alignRight=True, reset=False)
         await asyncio.sleep(1)
 
 async def display_disk(display):
     while True:
-        await display.display("Disk: {}/{} GB".format(stats.used_space(), stats.total_space()), 4)
-        await display.display("{}%".format(stats.percent_space()), 4, 20, alignRight=True, reset=False)
+        ratio_space_out = "Disk: {}/{} GB".format(stats.used_space(), stats.total_space())
+        percent_space_out = "{}%".format(stats.percent_space())
+        await display.display(ratio_space_out, 4)
+        await display.display(percent_space_out, 4, 20, alignRight=True, reset=False)
         await asyncio.sleep(60)
 
 async def createDisplay():
